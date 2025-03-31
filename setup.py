ae54ca44efd843bfc4d878419bae8e6773fbe310
Ranuja01 <ranuja.pinnaduwage@gmail.com>
2025-03-31 18:50:27 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     description="A profiling tool with AI-based suggestions for performance optimization",  # Package description
     long_description=open('README.md').read(),  # Read long description from README file
     long_description_content_type="text/markdown",  # Markdown format for README
-    url="https://github.com/yourusername/pyprofilerai",
+    url="https://github.com/Ranuja01/pyprofilerai",
     packages=find_packages(where="src"),  # Locate all packages under src/
     package_dir={"": "src"},
     classifiers=[
